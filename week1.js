23d32b46755592a3d7aec7ad8aecd066fb7a4c18
lwlsn <ewilson01@protonmail.ch>
2020-10-17 15:57:43 +0100

    week 1 commit

---
+++ week1.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  // What is a comment 
+  
+  createCanvas(400, 400); // What does changing this do 
+  
+  // print("Hello World");
+}
+
+function draw() {
+  // background(0); // What does this do ?? 
+  background(200, 25, 80); // Why is this now colourful? 
+  
+  fill(200, 255, 0);
+}
