6aee68756901e879cc354d0cb615d2ac99e0eff1
Lizzie <e-wilson1@hotmail.com>
2020-10-18 21:23:01 +0100

    Update week1.js

--- week1.js
+++ week1.js
@@ -1,15 +1,23 @@
-
+// What's the difference between setup and function? 
 function setup() {
   // What is a comment 
   
-  createCanvas(400, 400); // What does changing this do 
+  //Task 2 - Run the code and see what happens. 
+  // What does changing the numbers in the brackets below do?
+  createCanvas(400, 400); 
   
+  // Task 3 - remove the // below and run the code. See what happens
+  // when you change the value. 
   // print("Hello World");
 }
 
 function draw() {
-  // background(0); // What does this do ?? 
-  background(200, 25, 80); // Why is this now colourful? 
+  // Task 2 -  What does changing the number in the brackets below do? 
+  background(0); 
+  // background(200, 25, 80); // Why is this now colourful? 
   
-  fill(200, 255, 0);
+
+  //Task 4- move the print statement into here and see what happens. 
+  
+  // fill(200, 255, 0);
 }
