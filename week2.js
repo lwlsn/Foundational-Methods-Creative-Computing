8030f3f603708bb337ad6d7633a9b34dcdb5a11f
Lizzie <e-wilson1@hotmail.com>
2020-10-27 18:19:02 +0000

    Create week2.js

---
+++ week2.js
@@ -0,0 +1,27 @@
+// Week 2 - Drawing and Animation Basics. 
+
+function setup() {
+  createCanvas(400,400);
+  
+  // Why aren't we drawing shapes in here? What happens if we move our shapes in here?  
+}
+
+function draw() {
+  //Task 5a, give the background some colour. 
+  background(220);
+  
+  
+  //Task 5b - Use the fill and stroke function here. 
+  
+  // Task 2 - Draw a circle on the screen. 
+  // Write your own code to below to create an ellipse 
+  // ellipse();
+  
+  // Task 3 - Add a line here..
+  
+  // Task 4 - Add some other shapes below here..  
+  
+  
+  
+}
+
