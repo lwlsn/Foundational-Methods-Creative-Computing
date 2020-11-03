553cb0e687731a3af9b4268edf3f79c5375b0a28
Lizzie <e-wilson1@hotmail.com>
2020-11-03 12:00:58 +0000

    Create week3.js

---
+++ week3.js
@@ -0,0 +1,16 @@
+function setup() {
+  // task 2 - create canvas 
+}
+
+function draw() {
+   // task 3 - draw a blue background 
+  
+  
+  //task 4a+b - draw a green circle with a red outline
+  
+  //task 4c - now let's make the circle white again.  
+  
+  
+  
+  // task 5 - let's make the circle move with our mouse 
+}
