cf957605d20410a0d6893a6cadb49248e6f43b6c
Lizzie <e-wilson1@hotmail.com>
2020-11-24 15:12:20 +0000

    Create week6.js

---
+++ week6.js
@@ -0,0 +1,90 @@
+// Week 6 - Conditionals and the introduction to loops
+
+// Task 1 - Change the background colour according to where it is on the screen (halves then quadrants) 
+
+// Task 2 - If/else statement for mousePressed. If the mouse is pressed, draw a circle, otherwise draw a rectangle.  
+
+// Task 3 - Change the rectangle colour when the mouse is inside the square. 
+
+// Task 4 - Remove the previous  code  rectangle and circle by uncommenting it. Now, when the g key is pressed, turn the circle green. 
+
+
+// Task 5 - Add else if statements to turn the circle blue and red on pressing the b and r keys.
+
+
+// Task 6 - Using our key pressed function now, move the circle around the screen at a constant speed. 
+
+// Task 7 [Extension] - make a snake that you can move around the screen on pressing your keys by using a while or foor loop and repeating the circles at even intervals. 
+
+
+let circleX, circleY;
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  circleX = width/2;
+  circleY = height/2;
+  
+}
+
+function draw() {
+  
+  // Task 1 - write an if statement here to update the background depending on the mouse's x position on the screen. Uncomment the statements below and fill the code in 
+  
+//   if (mouseX < ) {
+//         // Task 1b - write another if statement inside to check our mouse's y position on the screen.
+  
+
+//   } 
+  
+//    if (mouseX >= ) { - can we use an else statement here? 
+//      //  Task 1b - write another if statement inside here
+
+//   }
+
+  
+ // Task 2- Now write an if/else statement for when our mouse is pressed. If the mouse is pressed, draw a circle, otherwise draw a rectangle. 
+   // if ( ) {
+  
+  // }
+  // else { 
+  
+  // Task 3 - If the mouse is over the rectangle, change the fill colour. 
+  
+// }
+
+  
+  // Task 4 - Remove the previous  code  rectangle and circle by uncommenting it. Now, when the g key is pressed, turn the circle green. 
+//     if ( ) {
+//         if (key == '') {
+    
+  
+//     // Task 5 - Add else if statements to turn the circle blue and red on pressing the b and r keys.
+      
+//       else if (key == '') {
+        
+//       }
+  
+//       }
+//     }
+  
+
+  
+}
+
+
+
+// Task 6 - Using our key pressed function now, move the circle around the screen at a constant speed. 
+// function keyPressed() {
+//   if (keyCode === ) {
+    
+//   } else if (keyCode === ) {
+   
+//   }
+//   if (keyCode === ) {
+   
+//   } else if (keyCode === RIGHT_ARROW) {
+
+//   }
+  
+// }
