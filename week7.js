5e1ca559dc1ebe0460b81d4672a978473e0935f5
Lizzie <e-wilson1@hotmail.com>
2020-11-30 18:35:35 +0000

    Create week7.js

---
+++ week7.js
@@ -0,0 +1,47 @@
+// Week 7 - Lab. 
+// This week we'll apply our knowledge of loops in our code and make sure that we understand how to use these to repeat an action. 
+
+// Task 1- fill in the background colours. 
+
+// Task 2 - Use our rect command to add more rectangles at 100 pixel intervals to fill in all of the grass area. 
+
+// Task 3 - Now replace this code with a for loop. 
+
+// Task 4 - Add a house at 25 pixels across, 350 pixels down. Fill it in dark brown. 
+
+//Task 5 - Use another for loop to create a row of houses. 
+
+// Task 6 - Use one final for loop to create a row of the roof on the houses. 
+
+ 
+function setup() {
+  createCanvas(500, 500);
+  noStroke();
+  
+  
+}
+
+function draw() {
+  // Task 1 - Fill in the sky blue. 
+  background(220);
+  
+  // Task 1 - Draw the sun- orange
+  noStroke();
+  ellipse(335, 66, 70, 70);
+  
+
+  // Task 1 - Draw the grass lawn green 
+  rect(0,400,100,100);
+  
+ 
+  // Task 2 - Add more rectangles to complete the grass. 
+  
+  
+  // Task 3 - Replace our code we just wrote with a for loop. 
+  
+  // Task 4 & 5 - Add another for loop for our houses. 
+  
+  // Task 6 - Add rooves to each of our houses. 
+  
+  
+}
