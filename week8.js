88cc63e2c21d76925483dba99acc6c02de26e019
Lizzie <e-wilson1@hotmail.com>
2020-12-07 18:11:34 +0000

    Create week8.js

---
+++ week8.js
@@ -0,0 +1,46 @@
+
+// Week 8 - basic snake game 
+
+// Global variables for our snake and food's 
+// x and y co-ordinates on the screen. 
+let snakeX, snakeY;
+let foodX, foodY;
+
+let speedX = 2;
+let speedY = 2;
+
+
+function setup() {
+  // Task 1a - change our canvas size to 500x500
+  createCanvas(500, 500);
+  noStroke();
+  
+  // Task 2a  - give random values to our snake and food variables
+//   snakeX = random(width);
+//   snakeY = 
+  
+//   foodX = ;
+//   foodY = ;
+  
+}
+
+function draw() {
+  background(220);
+  
+  // Task 1b - Write some text for our score here.
+  // fill(0);
+  // textSize();
+  // text();
+ 
+  //Task 2b - use our ellipse command with the variables here
+  
+  // Snake ellipse
+  // Task 3 - choose a colour for our snake
+  // ellipse(snakeX, , ,);
+  
+  
+  // Food ellipse
+  // Task 3 - choose a random colour for our food 
+  // ellipse(foodX, , ,);
+  
+}
