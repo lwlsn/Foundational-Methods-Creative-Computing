9b7946f5c89eb9338084c8f32e0644602639664c
Lizzie <e-wilson1@hotmail.com>
2020-12-07 18:24:44 +0000

    Update week8.js

--- week8.js
+++ week8.js
@@ -1,5 +1,5 @@
 
-// Week 8 - basic snake game 
+// Week 8  - basic snake game 
 
 // Global variables for our snake and food's 
 // x and y co-ordinates on the screen. 
@@ -7,7 +7,10 @@ let snakeX, snakeY;
 let foodX, foodY;
 
 let speedX = 2;
-let speedY = 2;
+let speedY = 0;
+
+
+// Task 8 [Extension] - Create a variable to keep track of the score, i.e. how many times our snake has eaten the food. 
 
 
 function setup() {
@@ -43,4 +46,56 @@ function draw() {
   // Task 3 - choose a random colour for our food 
   // ellipse(foodX, , ,);
   
+  
+  // Task 4 - Increment our circle across the canvas
+  // snakeX += ;
+  // snakeY += ;
+  
+  
+  // Task 5 - Create conditionals for when we reach the end of the screen. 
+  
+  
+//   if (snakeX > width) {
+//     snakeX = 0;
+//   }
+  
+//   if (snakeX < 0) {
+//     snakeX = width;
+//   }
+  
+//   if (snakeY > height){
+//     snakeY = 0;
+//   }
+  
+//   if (snakeY < 0) {
+//     snakeY = height;
+//   }
+  
+  // Task 7
+  // if (dist ( foodX,foodY, circleX, circleY) < 20) {
+  //     // Turn the circle black and stop it moving.
+  // }
+  
+  
+  
 }
+
+
+// Task 6 - When keys are pressed, change the direction of the circle on the screen. 
+// function keyPressed() {
+//    if (keyCode === ) {
+//     speedX = ;
+//     speedY = ;
+//   } else if (keyCode === ) {
+//     speedX = ;
+//     speedY = ;
+//   }
+//   else if (keyCode === ) {
+//     speedX = -2;
+//     speedY = 0;
+//   } else if (keyCode === ) {
+//     speedX = 2;
+//     speedY = 0;
+//   }
+  
+// }
