96ea3d24e0645a09dfb79566f78f127a91c420a1
lwlsn <ewilson01@protonmail.ch>
2021-11-01 16:55:49 +0000

    remove full files, update week 6

--- week6.js
+++ week6.js
@@ -1,92 +1,63 @@
-// Week 6 - Conditionals and the introduction to loops
 
-// Task 1 - Change the background colour according to where it is on the screen (halves then quadrants) 
+// Week 6 - lab
 
-// Task 2 - If/else statement for mousePressed. If the mouse is pressed, draw a circle, otherwise draw a rectangle.  
+// Task 1 -  Give the variables an initial value. 
+let xPos=200;
+let yPos=200;
+let size=50;
 
-// Task 3 - Change the rectangle colour when the mouse is inside the square. 
+// Task 2a - Create a variable for speed.
+// Task 2c - Separate into x-speed, y-speed. 
 
-// Task 4 - Remove the previous  code  rectangle and circle by uncommenting it. Now, when the g key is pressed, turn the circle green. 
-
-
-// Task 5 - Add else if statements to turn the circle blue and red on pressing the b and r keys.
-
-
-// Task 6 - Using our key pressed function now, move the circle around the screen at a constant speed. 
-
-// Task 7 [Extension] - make a snake that you can move around the screen on pressing your keys by using a while or foor loop and repeating the circles at even intervals. 
-
-
-let circleX, circleY;
 
 function setup() {
   createCanvas(400, 400);
-  background(220);
-  circleX = width/2;
-  circleY = height/2;
-  
 }
 
 function draw() {
-  
-  // Task 1 - write an if statement here to update the background depending on the mouse's x position on the screen. Uncomment the statements below and fill the code in 
-  
-//   if (mouseX < ) {
-//         // Task 1b - write another if statement inside to check our mouse's y position on the screen.
-  
-
-//   } 
-  
-//    if (mouseX >= ) { - can we use an else statement here? 
-//      //  Task 1b - write another if statement inside here
-
-//   }
-
-  
- // Task 2- Now write an if/else statement for when our mouse is pressed. If the mouse is pressed, draw a circle, otherwise draw a rectangle. 
-   // if ( ) {
-  
-  // }
-  // else { 
-  
-  // Task 3 - If the mouse is over the rectangle, change the fill colour. 
-  
-// }
-
-  
-  // Task 4 - Remove the previous  code  rectangle and circle by uncommenting it. Now, when the g key is pressed, turn the circle green. 
-//     if ( ) {
-//         if (key == '') {
     
-  
-//     // Task 5 - Add else if statements to turn the circle blue and red on pressing the b and r keys.
-      
-//       else if (key == '') {
-        
-//       }
-  
-//       }
-//     }
-  
-  
-  // ellipse(circleX, circleY, 100, 100 );
+  // Task 5 + 6, change the background of the canvas using an if/else statement. 
+  background(220);
 
+  
+  noStroke();
+  // Task 8 - Extension, create four circles next to each other using a for loop. Make sure these all move on the arrow keys.
+  ellipse(xPos, yPos, size, size);
+  
+
+  // Task 2b, 2c increment the x and y by variable speeds. 
+  // xPos = xPos +;
+  // yPos = yPos +;
+  
+
+// Task 3a - check the edge conditions for the x-position. 
+// Task 4a - add an OR operator to check both conditions.
+//   if (xPos > ) {
+        
+//   }
+  
+  
+// Task 3b - check the edge conditions for the y-position.
+// Task 4b - add an OR operator to check both conditions.
+//   if (yPos > ) {
+    
+// }
+  
   
 }
 
 
-
-// Task 6 - Using our key pressed function now, move the circle around the screen at a constant speed. 
-// function keyPressed() {
-//   if (keyCode === ) {
-    
-//   } else if (keyCode === ) {
-   
-//   }
-//   if (keyCode === ) {
-   
-//   } else if (keyCode === ) {
-
-//   }
+// Task 7  - Extension
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+      // move circle up
+  } else if (keyCode === DOWN_ARROW) {
+     // move circle down
+  }
+  if (keyCode === LEFT_ARROW) {
+     // move circle left
+  } else if (keyCode === RIGHT_ARROW) {
+     // move circle right
+  }
   
-// }
+}
